1f00c3223184d1c6fac860a952ac2c5031278d45
liujuping <1052228950@qq.com>
2016-03-24 09:57:42 +0800

    solve the bug to num

--- 2step/js/task18.js
+++ 2step/js/task18.js
@@ -20,12 +20,13 @@ function on(element,eventName,listener) {
 
 // 事件代理，为四个按钮绑定事件.
 on(btn_box,'click',function(e){
-    if(!str || str>100 || str<0 || /[^0-9]/.test(str)) {
+    var str=ipt.value;
+
+        if(!str || parseInt(str)>100 || parseInt(str)<0 || /[^0-9]/.test(str)) {
         alert('请输入0-100之间的数字');
         return;
     }
-    var str=parseInt(ipt.value),
-        p=document.createElement('div'),
+    var p=document.createElement('div'),
         text=document.createTextNode(str);
     p.appendChild(text);
 
