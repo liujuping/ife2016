3c5fef66296efeeb9a4cabba0b54f51946ecea1a
liujuping <1052228950@qq.com>
2016-04-05 12:19:50 +0800

    change detail

--- 2step/js/task20.js
+++ 2step/js/task20.js
@@ -11,53 +11,53 @@ var arr=[],
 
 //事件绑定
 function on(element,eventName,listener) {
-    if(element.addEventListener) {
-        element.addEventListener(eventName,listener,false);
+    if (element.addEventListener) {
+        element.addEventListener(eventName, listener, false);
     }
-    else if(element.attachEvent) {
-        element.attachEvent('on'+eventName,listener);
+    else if (element.attachEvent) {
+        element.attachEvent('on' + eventName, listener);
     }
     else {
-        element['on'+eventName]=listener;
+        element['on' + eventName] = listener;
     }
 }
 
 //绑定点击事件
-on(start_search,'click',function(e){
-    for(var i=0; i< arr.length ; i++) {      //设置的颜色还原
-        box.children[i].style.backgroundColor='red';
+on(start_search,'click',function(e) {
+    var i= 0,
+        rg = new RegExp(str),      //正则表达式设置模糊查询
+        str = search_ipt.value;
+    for (i = 0; i < arr.length; i++) {      //设置的颜色还原
+        box.children[i].style.backgroundColor = 'red';
     }
-
-    var str=search_ipt.value;
-    if(!str || str=='') {
+    if (!str || str == '') {
         alert('查询内容为空');
         return 0;
     }
-    var rg = new RegExp(str);      //正则表达式设置模糊查询
-    for(var i=0; i< arr.length ; i++) {
-        if(rg.test(arr[i])) {           //设置满足条件的颜色为blue
-            box.children[i].style.backgroundColor='blue';
+    for (i = 0; i < arr.length; i++) {
+        if (rg.test(arr[i])) {           //设置满足条件的颜色为blue
+            box.children[i].style.backgroundColor = 'blue';
         }
     }
 
 });
 
 // 事件代理，为四个按钮绑定事件.
-on(btn_box,'click',function(e){
-    var str=ipt.value,
-        i= 0,
-        strArr=str.split(/[ ,，、\n\t]/);   //用分割符分别存入数组
+on(btn_box,'click',function(e) {
+    var str = ipt.value,
+        i = 0,
+        strArr = str.split(/[ ,，、 \n\t]/);   //用分割符分别存入数组
 
-    for(i=0;i<strArr.length;i++) {     //删除空的数组
-        if(/^ *$/.test(strArr[i])) {
-            strArr.splice(i,1);
+    for (i = 0; i < strArr.length; i++) {     //删除空的数组
+        if (/^ *$/.test(strArr[i])) {
+            strArr.splice(i, 1);
             i--;
         }
     }
 
     //循环把数据都放在arr
-    for(i in strArr) {
-        switch (e.target.id){
+    for (i in strArr) {
+        switch (e.target.id) {
             case 'left_in' :
                 arr.unshift(strArr[i]);
                 break;
@@ -77,10 +77,10 @@ on(btn_box,'click',function(e){
 });
 
 // 显示数据
-function show(){
-    var text='';
-    for(var index in arr) {
-        text+='<div>'+arr[index]+'</div>'
+function show() {
+    var text = '';
+    for (var index in arr) {
+        text += '<div>' + arr[index] + '</div>'
     }
-    box.innerHTML=text;
+    box.innerHTML = text;
 }
--- 2step/js/task21.js
+++ 2step/js/task21.js
@@ -9,15 +9,15 @@ var tag_box=document.getElementById('tag_box'),
     lover_box=document.getElementById('lover_box');
 
 //定义ShowTag构造器
-function ShowTag(ipt,box){
-    this.arr=[];      //存放数组
-    this.box=box;     //显示tag的容器
-    this.ipt=ipt;     //输入框
-    this.length=10;   //显示的tag的数目
+function ShowTag(ipt,box) {
+    this.arr = [];      //存放数组
+    this.box = box;     //显示tag的容器
+    this.ipt = ipt;     //输入框
+    this.length = 10;   //显示的tag的数目
 }
 
 //绑定事件函数
-function on(element,eventName,listener){
+function on(element,eventName,listener) {
     if (element.addEventListener) {
         element.addEventListener(eventName, listener, false);
     }
@@ -30,10 +30,10 @@ function on(element,eventName,listener){
 }
 
 //ShowTag构造器方法
-ShowTag.prototype={
+ShowTag.prototype= {
     // 去重
-    trim:function(){
-        var i=0, j=0;
+    trim: function () {
+        var i = 0, j = 0;
         for (; i < this.arr.length; i++) {      //判断重复，如果元素重复就去掉该元素
             for (j = i + 1; j < this.arr.length; j++) {
                 if (this.arr[i] == this.arr[j]) {
@@ -42,27 +42,27 @@ ShowTag.prototype={
                 }
             }
         }
-        while(this.arr.length>this.length) {
+        while (this.arr.length > this.length) {
             this.arr.shift();
         }
         this.show();//去重后重新显示标签
         return this;
     },
     //显示标签
-    show:function(){
+    show: function () {
         var text = '';
-        for (var index=0 ; index<this.arr.length;index++) {
-            text += '<div data-num="'+index+'" class="item"><span>点击删除</span>' + this.arr[index] + '</div>';
+        for (var index = 0; index < this.arr.length; index++) {
+            text += '<div data-num="' + index + '" class="item"><span>点击删除</span>' + this.arr[index] + '</div>';
         }
         this.box.innerHTML = text;
         return this;
     },
     //将输入的值添加到数组中
-    add:function(){
+    add: function () {
         str = this.ipt.value.split(/[ ,、， \n\t]/);   //回车，逗号（全角半角均可），顿号，空格（全角半角、Tab等均可）等符号作为间隔
-        for(var i=0 ;i<str.length;i++) {
+        for (var i = 0; i < str.length; i++) {
             var item = str[i];
-            if(item=='') {     //去掉空元素
+            if (item == '') {     //去掉空元素
             }
             else {
                 this.arr.push(item);
@@ -72,22 +72,22 @@ ShowTag.prototype={
         return this;
     },
     //点击元素删除
-    deleteEvent:function(e){
+    deleteEvent: function (e) {
         //事件代理，判断点击的元素
-        var item = e.target.className=='item'? e.target : e.target.parentNode.className=='item'? e.target.parentNode : null;
-        if(item==null) {
+        var item = e.target.className == 'item' ? e.target : e.target.parentNode.className == 'item' ? e.target.parentNode : null;
+        if (item == null) {
             return 0;
         }
         //删除第n个元素，之后重新显示元素
-        this.arr.splice(item.getAttribute('data-num'),1);
+        this.arr.splice(item.getAttribute('data-num'), 1);
         this.show();
     }
 };
 
 //定义TagIpt构造器
-function TagIpt(tag_ipt,tag_box){
+function TagIpt(tag_ipt,tag_box) {
     // 调用父类构造器, 确保(使用Function#call)"this" 在调用过程中设置正确
-    ShowTag.call(this,tag_ipt,tag_box);
+    ShowTag.call(this, tag_ipt, tag_box);
 }
 
 // 建立一个由ShowTag.prototype继承而来的TagIpt.prototype对象.
@@ -98,38 +98,38 @@ TagIpt.prototype.constructor=ShowTag;
 //定义TagArea构造器
 function TagArea(lover_area,lover_box,btn) {
     //TagArea的特有属性
-    this.btn=btn;
-    ShowTag.call(this,lover_area,lover_box);
+    this.btn = btn;
+    ShowTag.call(this, lover_area, lover_box);
 }
 
 TagArea.prototype=Object.create(ShowTag.prototype);
 TagArea.prototype.constructor=ShowTag;
 
-TagIpt.prototype.init=function(){
+TagIpt.prototype.init=function() {
     //绑定事件
-    on(this.box,'click',this.deleteEvent.bind(this));//删除元素事件的绑定
-    on(this.ipt,'keyup',this.keyUp.bind(this));    //输入框输入内容事件的绑定
-    on(this.ipt,'keydown',this.preventDefault);    //阻止输入框的默认事件
+    on(this.box, 'click', this.deleteEvent.bind(this));//删除元素事件的绑定
+    on(this.ipt, 'keyup', this.keyUp.bind(this));    //输入框输入内容事件的绑定
+    on(this.ipt, 'keydown', this.preventDefault);    //阻止输入框的默认事件
 };
-TagIpt.prototype.keyUp=function(e){
-    if(e.keyCode==188 || e.keyCode==32 || e.keyCode=='13') {
+TagIpt.prototype.keyUp=function(e) {
+    if (e.keyCode == 188 || e.keyCode == 32 || e.keyCode == '13') {
         this.add();
-        this.ipt.value='';
+        this.ipt.value = '';
     }
 };
-TagIpt.prototype.preventDefault=function(e){
-    if(e.keyCode=='13') {
-        e.preventDefault ? e.preventDefault() : e.returnValue=false;
+TagIpt.prototype.preventDefault=function(e) {
+    if (e.keyCode == '13') {
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
     }
 };
 
-TagArea.prototype.init=function(){
-    on(this.box,'click',this.deleteEvent.bind(this));
-    on(this.btn,'click',this.click.bind(this));//按钮点击事件绑定
+TagArea.prototype.init=function() {
+    on(this.box, 'click', this.deleteEvent.bind(this));
+    on(this.btn, 'click', this.click.bind(this));//按钮点击事件绑定
 };
-TagArea.prototype.click=function(){
+TagArea.prototype.click=function() {
     this.add();
-    this.ipt.value='';
+    this.ipt.value = '';
 };
 
 var tagIpt = new TagIpt(tag_ipt,tag_box);
