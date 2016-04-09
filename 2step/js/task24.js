6162468e7e41e2ec74fd403d5e8ecf27069ff6b5
liujuping <1052228950@qq.com>
2016-04-09 10:35:32 +0800

    change task 24 simple more

--- 2step/js/task24.js
+++ 2step/js/task24.js
@@ -7,20 +7,13 @@ var tree_box = document.getElementById('root'),
     btn_speed = document.getElementById('btn_speed'),
     ipt=document.getElementById('ipt'),
     ipt_text=document.getElementById('ipt_text');
-/*var nodeObj = {
- value:root,
- children:[{},{},{}],
- text:''
- };*/
+
 function Tree(box) {
     this.box = box;
-    this.nodeObj = {};
     this.speed = 500;
     this.showArr = [];
     this.timer = null;
     this.selectNode=null;
-
-    this.init();
 }
 
 //绑定事件函数
@@ -37,9 +30,6 @@ function on(element,eventName,listener) {
 }
 
 Tree.prototype = {
-    init: function () {
-        this.nodeObj = this.initNode(this.box);
-    },
     starEvent: function (Event) {
         //判断是否现在正在动画，如果在动画，点击没有效果
         if (this.timer != null) {
@@ -47,25 +37,24 @@ Tree.prototype = {
             return 0;
         }
         //清空之前的样式
-        this.deepFirst(this.nodeObj);
         this.clear();
         //初始化showArr为空
         this.showArr = [];
         switch (Event) {
             case 'deepFirst':
-                this.deepFirst(this.nodeObj);
+                this.deepFirst(this.box);
                 this.show();
                 break;
             case 'breadthFirst':
-                this.breadthFirst(new Array(this.nodeObj));
+                this.breadthFirst(this.box);
                 this.show();
                 break;
             case 'deepFirstSearch':
-                this.deepFirst(this.nodeObj);
+                this.deepFirst(this.box);
                 this.find();
                 break;
             case 'breadthFirstSearch':
-                this.breadthFirst(new Array(this.nodeObj));
+                this.breadthFirst(this.box);
                 this.find();
                 break;
             default :
@@ -75,7 +64,7 @@ Tree.prototype = {
     clear: function () {
         //点击之后把之前的样式清除
         for (var i = 0; i < this.showArr.length; i++) {
-            this.showArr[i].value.className = '';
+            this.showArr[i].className = '';
         }
         this.selectNode=null;
     },
@@ -91,7 +80,7 @@ Tree.prototype = {
         this.timer = setInterval((function () {
             //将上一次遍历的内容样式去除
             if (i) {
-                this.showArr[i - 1].value.className = '';
+                this.showArr[i - 1].className = '';
             }
             //如果遍历到最后一个元素之后，即没有找到，停止timer，提示没有该内容
             if (i == this.showArr.length) {
@@ -101,9 +90,9 @@ Tree.prototype = {
                 return;
             }
             //设置正在查找的元素的className
-            this.showArr[i].value.className = 'select';
+            this.showArr[i].className = 'select';
             //如果找到元素，停止循环，
-            if (str == this.showArr[i].text) {
+            if (str == this.showArr[i].children[0].textContent) {
                 alert('find it');
                 clearInterval(this.timer);
                 this.timer = null;
@@ -112,41 +101,27 @@ Tree.prototype = {
             i++;
         }).bind(this), this.speed);
     },
-    //初始化节点
-    initNode: function (node) {
-        var nodeObj = {},
-            children = node.children,
-            i;
-        nodeObj.children = [];
-        nodeObj.value = node;
-        nodeObj.text = children[0].innerText;
-        for (i = 1; i < children.length; i++) {
-            nodeObj.children.push(this.initNode(children[i]));
-        }
-        return nodeObj;
-    },
     //深度遍历
-    deepFirst: function (nodeObj) {
-        var i;
-        if (nodeObj != null) {
-            this.showArr.push(nodeObj);
-            for (i = 0; i < nodeObj.children.length; i++) {
+    deepFirst: function (box) {
+        if (box != null) {
+            this.showArr.push(box);
+            for (var i = 1; i < box.children.length; i++) {
                 //递归函数
-                this.deepFirst(nodeObj.children[i]);
+                this.deepFirst(box.children[i]);
             }
         }
     },
     //广度遍历
-    breadthFirst: function (parents) {
+    breadthFirst: function (box) {
         //parent存放父节点，取其子节点，放在数组后面，待之后将子节点做父节点用
-        var parent = parents.shift(),
-            i;
+        var parent = box,  //当前父元素
+            parents=[];    //还没有用的元素
         do {
             this.showArr.push(parent);
-            for (i = 0; i < parent.children.length; i++) {
+            for (var i = 1; i < parent.children.length; i++) {
                 parents.push(parent.children[i]);
             }
-            parent = parents.shift();
+            parent = parents.shift();  //取下一个父元素
         }
         while (parent);
     },
@@ -155,14 +130,14 @@ Tree.prototype = {
         //定时器遍历数组
         this.timer = setInterval((function () {
             if (i) {
-                this.showArr[i - 1].value.className = '';
+                this.showArr[i - 1].className = '';
             }
             if (i == this.showArr.length) {
                 clearInterval(this.timer);
                 this.timer = null;
                 return;
             }
-            this.showArr[i].value.className = 'select';
+            this.showArr[i].className = 'select';
             i++;
         }).bind(this), this.speed);
     },
@@ -173,11 +148,9 @@ Tree.prototype = {
     selectNodeFun:function(node){
         //清除搜索时留下的样式
         this.clear();
-        if(this.selectNode) {
-            this.selectNode.className='';
-        }
         this.selectNode=node;
         this.selectNode.className='select';
+        this.showArr.push(this.selectNode);
     },
     deleteNode:function(){
         if(this.selectNode==null) {
@@ -185,11 +158,10 @@ Tree.prototype = {
         }
         else {
             this.selectNode.parentNode.removeChild(this.selectNode);
-            this.nodeObj=this.initNode(this.box);
         }
     },
-    addNode:function(txt){
-        console.log(txt);
+    addNode:function(ipt){
+        var txt=ipt.value;
         if(txt=='') {
             alert('节点内容不能为空！');
         }
@@ -203,15 +175,13 @@ Tree.prototype = {
             div.appendChild(span);
             span.appendChild(text);
             this.selectNode.appendChild(div);
-            //重新初始化节点树
-            this.nodeObj=this.initNode(this.box);
+            ipt.value='';
         }
 
     }
 };
 
 var tree=new Tree(tree_box);
-
 
 //绑定删除节点的事件
 on(change_node,'click',function(e){
@@ -220,8 +190,7 @@ on(change_node,'click',function(e){
             tree.deleteNode();
             break;
         case 'addNode':
-            var text=ipt_text.value;
-            tree.addNode(text);
+            tree.addNode(ipt_text);
             break;
 
     }
